143ab857591c82ccc2faec056a034b4f9519c4b3
sheikh-Zulkifal <sheikhzulkifal1@gmail.com>
2025-04-20 22:54:24 +0500

    first

---
+++ server.js
@@ -0,0 +1,18 @@
+// server.js
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware (optional)
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
